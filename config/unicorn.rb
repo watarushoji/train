56894f685c883a52e1b01dbeab77c786578a3597
wataru_shoji <clear.refresh100@gmail.com>
2014-11-28 15:32:49 +0900

    d

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,12 +1,34 @@
+application = 'boobs'
 APP_PATH = "/usr/share/nginx/html/boobs/current"
 
 working_directory 	APP_PATH
-pid 				APP_PATH + "/tmp/pids/unicorn.pid"
+
 stderr_path 		APP_PATH + "/log/unicorn.log"
 stdout_path 		APP_PATH + "/log/unicorn.log"
 
-listen 				"/tmp/unicorn.sock"
+listen "/var/run/unicorn_#{application}.sock"   # Unix Domain Socket
+pid "/var/run/unicorn_#{application}.pid"     #PIDファイル出力先
 #listen 3000
+
 worker_processes 2
 timeout 30
 preload_app true
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+    if old_pid != server.pid
+      begin
+        sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+        Process.kill(sig, File.read(old_pid).to_i)
+      rescue Errno::ENOENT, Errno::ESRCH
+      end
+    end
+ 
+    sleep 1
+  end
+ 
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
