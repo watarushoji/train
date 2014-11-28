a2fbf01aa46c86f7b18a4e69a6408942252d80e2
wataru_shoji <clear.refresh100@gmail.com>
2014-11-28 13:59:27 +0900

    Unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,10 @@
-working_directory "/var/www/html"
-pid "/var/www/html/tmp/pids/unicorn.pid"
-stderr_path "/var/www/html/log/unicorn.log"
-stdout_path "/var/www/html/log/unicorn.log"
+working_directory 	"/usr/share/nginx/html/boobs"
+pid 				"/usr/share/nginx/html/boobs/tmp/pids/unicorn.pid"
+stderr_path 		"/usr/share/nginx/html/boobs/log/unicorn.log"
+stdout_path 		"/usr/share/nginx/html/boobs/log/unicorn.log"
 
-listen "/tmp/unicorn.todo.sock"
+listen 				"/usr/share/nginx/html/boobs/tmp/unicorn.sock"
 #listen 3000
 worker_processes 2
 timeout 30
+preload_app true
