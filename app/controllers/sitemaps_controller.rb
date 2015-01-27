c3e1d069f144cac6263f539dc14fabf4411fc8b6
wataru_shoji <clear.refresh100@gmail.com>
2015-01-27 22:29:06 +0900

    d

--- app/controllers/sitemaps_controller.rb
+++ app/controllers/sitemaps_controller.rb
@@ -1,5 +1,6 @@
 class SitemapsController < ApplicationController
   def sitemaps
-  	@sitemaparticles = Article.all.order("created_at DESC")
+  	@sitemaparticles = Article.all.order("created_at DESC").limit(5)
+  	@sitemappopulars = Article.all.order("pv DESC").limit(5)
   end
 end
